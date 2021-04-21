e7bc1b4e2e6e83ab147f3b97e6b9f02aa2936564
daQuincy <yuquan95@gmail.com>
2021-04-21 19:48:00 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,59 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import { utils } from 'ethers';
-import getBlockchain from './ethereum.js';
+import { useState, useEffect } from 'react';
+import { ethers, Contract , utils} from 'ethers';
+import Pluto from './contracts/Pluto.json';
+
+// if use this, it does not connect to metamask after build
+//const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+// https://stackoverflow.com/questions/60785630/how-to-connect-ethers-js-with-metamask
+let provider;
+window.ethereum.enable().then(provider = new ethers.providers.Web3Provider(window.ethereum));
+const signer = provider.getSigner();
+
+const pluto = new Contract(
+  "0xB22dee4B962303D42b4fC04B0eEc7429CE512239",
+  Pluto.abi,
+  signer
+);
 
 function App() {
-  const [pluto, setPluto] = useState(undefined);
+  const [beneficiary, setBeneficiary] = useState(undefined);
+  const [contribution, setContribution] = useState(undefined);
 
-  useEffect(() => {
-    const init = async () => {
-      const { pluto } = await getBlockchain();
-      setPluto(pluto);
-    };
-    init();
-  }, []);
-
-  const buy = async e => {
-    e.preventDefault();
-    const address = e.target.elements[0].value;
-    const contribution = utils.parseEther(e.target.elements[1].value);
-    const tx = await pluto.buyPresalesTokens(address, {gasLimit: 500000, value: contribution});
-    await tx.wait();
-  };
-
-  if(
-    typeof pluto === 'undefined'
-  ) {
-    return 'Loading...';
+  const buyPresalesTokens = async () => {
+    await pluto.buyPresalesTokens(beneficiary, {value: contribution});
   }
 
+  const handleBeneficiary = async (e) => {
+    setBeneficiary(e.target.value);
+  }
+
+  const handleContribution = async (e) => {
+    const contribution = utils.parseEther(e.target.value);
+    setContribution(contribution);
+  }
+  
   return (
-    <div className='container'>
-      <div className='row'>
+    <div className="App">
+      <h1>Presales</h1>
+      <br></br>
 
-        <div className='col-sm-6'>
-          <h2>Buy Presales Tokens</h2>
-          <form className="form-inline" onSubmit={e => buy(e)}>
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Address"
-            />
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Contribution"
-            />
-            <button 
-              type="submit" 
-              className="btn btn-primary"
-            >
-              Submit
-            </button>
-          </form>
-        </div>
+      <form>
+        <label>
+          Beneficiary Address:
+          <input type="text" onChange={handleBeneficiary} />
+        </label>
+        <label>
+          BNB to contribute:
+          <input type="text" onChange={handleContribution} />
+        </label>
+      </form>
 
+
+      <div>
+        <h2>Buy Presales Tokens:</h2>
+        <button onClick={buyPresalesTokens} className="btn-primary">Buy</button>
       </div>
+      
+
     </div>
   );
 }
