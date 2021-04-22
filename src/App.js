513cff7854158d3fdd234b24f1494d4d66d8db22
daQuincy <yuquan95@gmail.com>
2021-04-22 17:15:02 +0800

    refund button

--- src/App.js
+++ src/App.js
@@ -8,30 +8,36 @@ import 'semantic-ui-css/semantic.min.css'
 //const provider = new ethers.providers.Web3Provider(window.ethereum);
 
 // https://stackoverflow.com/questions/60785630/how-to-connect-ethers-js-with-metamask
-let provider;
-window.ethereum.enable().then(provider = new ethers.providers.Web3Provider(window.ethereum, "any"));
-const signer = provider.getSigner();
+let provider, signer, pluto;
 
-const pluto = new Contract(
-  "0xB22dee4B962303D42b4fC04B0eEc7429CE512239",
-  Pluto.abi,
-  signer
-);
+if (window.ethereum === undefined) {
+  provider = undefined;
+  signer = undefined;
+  pluto = undefined;
+} else {
+  window.ethereum.enable().then(provider = new ethers.providers.Web3Provider(window.ethereum, "any"));
+}
+
 
 function App() {
   const [connection, setConnection] = useState(false);
   const [totalContribution, setTotalContribution] = useState(0);
-  const [chainID, setChainId] = useState(undefined);
+  const [chainID, setChainID] = useState(undefined);
+
   const [allowBuy, setAllowBuy] = useState(false);
   const [presalesStart, setPresalesStart] = useState(false);
   const [presalesEnd, setPresalesEnd] = useState(false);
+  const [capReached, setCapReached] = useState(false);
+  const [indvCap, setIndvCap] = useState(false);
 
   const [beneficiary, setBeneficiary] = useState(undefined);
   const [contribution, setContribution] = useState(0);
+  const [contributed, setContributed] = useState(0);
   const [valBeneficiary, setValBeneficiary] = useState(false);
   const [valContribution, setValContribution] = useState(false);
 
-  const [buttonLoading, setButtonLoading] = useState(false);
+  const [buyButtonLoading, setBuyButtonLoading] = useState(false);
+  const [refundButtonLoading, setRefundButtonLoading] = useState(false);
 
   const [address, setAddress] = useState(undefined);
   const [txnLink, setTxnLink] = useState(undefined);
@@ -39,44 +45,67 @@ function App() {
   const [tokensAmount, setTokenAmounts] = useState(0);
 
   useEffect(() => {
-    const init = setInterval(async () => {
-      const _chainID = (await provider.getNetwork())["chainId"];
-      if (_chainID === 97 || _chainID === 56){
-        setConnection(true);
-        const _address = await signer.getAddress();
+    const load = setInterval(async () => {
+      if (provider !== undefined) {
+        const _chainID = (await provider.getNetwork())["chainId"];
+        if (_chainID === 97 || _chainID === 56){
+          setConnection(true);
+          const _address = await signer.getAddress();
 
-        let _tokensAmount = await pluto.getTokens(_address);
-        _tokensAmount = _tokensAmount.div(1000000000);
+          let _tokensAmount = await pluto.getTokens(_address);
+          _tokensAmount = utils.formatUnits(_tokensAmount, 9);
 
-        let _totalContribution = await pluto.weiRaised();
-        _totalContribution = utils.formatEther(_totalContribution);
+          let _contributed = await pluto.getContribution(_address);
+          _contributed = utils.formatEther(_contributed);
 
-        const _presalesStart = await pluto.getPresalesStarted();
-        const _presalesEnd = await pluto.getPresalesEnded();
+          let _totalContribution = await pluto.weiRaised();
+          _totalContribution = utils.formatEther(_totalContribution);
 
-        const _allowBuy = _presalesStart && !_presalesEnd;
+          const _presalesStart = await pluto.getPresalesStarted();
+          const _presalesEnd = await pluto.getPresalesEnded();
+          const _capReached = await pluto.capReached();
 
-        setTokenAmounts(_tokensAmount.toString());
-        setTotalContribution(_totalContribution);
-        setAddress(_address);
-        setChainId(_chainID);
-        setPresalesStart(_presalesStart);
-        setPresalesEnd(_presalesEnd);
-        setAllowBuy(_allowBuy);
+          const _allowBuy = _presalesStart && !_presalesEnd;
+
+          setTokenAmounts(_tokensAmount);
+          setContributed(_contributed);
+          setTotalContribution(_totalContribution);
+          setAddress(_address);
+          setPresalesStart(_presalesStart);
+          setPresalesEnd(_presalesEnd);
+          setAllowBuy(_allowBuy);
+          setCapReached(_capReached);
+        } else {
+          setConnection(false);
+        }
       } else {
-        setConnection(false)
+        setConnection(false);
       }
     }, 1000);
-    return () => clearInterval(init);
+
+    const init = async () => {
+      const _chainID = (await provider.getNetwork())["chainId"];
+      signer = provider.getSigner();
+
+      pluto = new Contract(
+        Pluto.networks[_chainID].address,
+        Pluto.abi,
+        signer
+      );
+
+      setChainID(_chainID);
+    }
+
+    init();
+
+    return () => clearInterval(load);
   }, []);
 
   const buyPresalesTokens = async (e) => {
     e.preventDefault();
-    setButtonLoading(true);
+    setBuyButtonLoading(true);
     const tx = await pluto.buyPresalesTokens(beneficiary, {gasLimit: 500000, value: contribution});
     await tx.wait();
-    let _tokensAmount = await pluto.getTokens(address);
-    _tokensAmount = _tokensAmount.div(1000000000);
 
     let txLink;
     if (chainID === 97) {
@@ -84,10 +113,31 @@ function App() {
     } else if (chainID === 56) {
       txLink = "https://bscscan.com/tx/" + tx.hash;
     };
+    setTxnHash(tx.hash);
+    setTxnLink(txLink);
+    setBuyButtonLoading(false);
+  }
+
+  const refundCapNotReached = async (e) => {
+    e.preventDefault();
+    setRefundButtonLoading(true);
+    const tx = await pluto.refund(beneficiary, {gasLimit: 500000});
+    await tx.wait();
+
+    let txLink;
+    if (chainID === 97) {
+      txLink = "https://testnet.bscscan.com/tx/" + tx.hash;
+    } else if (chainID === 56) {
+      txLink = "https://bscscan.com/tx/" + tx.hash;
+    };
+
+    let _tokensAmount = await pluto.getTokens(address);
+    _tokensAmount = utils.formatUnits(_tokensAmount, 9);
+
     setTokenAmounts(_tokensAmount.toString());
     setTxnHash(tx.hash);
     setTxnLink(txLink);
-    setButtonLoading(false);
+    setRefundButtonLoading(false);
   }
 
   const handleBeneficiary = (e) => {
@@ -108,16 +158,24 @@ function App() {
     }
   }
 
-  const handleContribution = (e) => {
+  const handleContribution = async (e) => {
     let _contribution = e.target.value;
     try {
       if (_contribution <= 0 || _contribution > 0.5) {
         setValContribution(false);
         setContribution(0);
       } else {
-        const contribution = utils.parseEther(e.target.value);
+        const _contributed = await pluto.getContribution(beneficiary);
+        const _individualCap = parseFloat(_contribution) + parseFloat(_contributed);
+        if (_individualCap > 0.5) {
+          setIndvCap(true);
+        } else {
+          setIndvCap(false);
+        }
+        _contribution = utils.parseEther(_contribution);
+        
         setValContribution(true);
-        setContribution(contribution);
+        setContribution(_contribution);
       }
     } catch {
       setValContribution(false);
@@ -136,7 +194,7 @@ function App() {
       <Message info hidden={!presalesEnd} header="Presales has already ended" />
       <Message header={"Current Total Contribution"} content={totalContribution + " BNB"} />
  
-      <Form onSubmit={buyPresalesTokens}>
+      <Form>
         <Form.Field>
             <label>Beneficiary Address</label>
             <Input
@@ -150,15 +208,25 @@ function App() {
               placeholder="MAX: 0.5 BNB"
               label="BNB"
               labelPosition="right"
+              disabled={presalesEnd}
             />
-            <Button primary disabled={(!valContribution || !valBeneficiary || !allowBuy || !connection)} loading={buttonLoading}>
+
+            <Button primary disabled={(!valContribution || !valBeneficiary || !allowBuy || !connection)} loading={buyButtonLoading} onClick={buyPresalesTokens}>
               Buy Tokens!
+            </Button>
+
+            <Button primary disabled={!presalesEnd && !capReached && !valBeneficiary} loading={refundButtonLoading} onClick={refundCapNotReached}>
+              Refund
             </Button>
         </Form.Field>
       </Form>
-
+      <Message hidden={!indvCap} error={true} header="Exceed individual limit!" content={"This transaction will fail because you exceeded individual limit. Enter a lower amount"} />
       <h3>
         Tokens you bought: {tokensAmount}
+      </h3>
+
+      <h3>
+        BNB contributed: {contributed}
       </h3>
 
       <h3>
